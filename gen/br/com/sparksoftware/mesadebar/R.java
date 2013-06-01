e5feb6a4707c94a3ae7ece8ea335c527b9c2d36e
HenriqueLacerda <geraldohenriquelacerda@gmail.com>
2013-06-01 14:04:11 -0300

    Outras classes

--- gen/br/com/sparksoftware/mesadebar/R.java
+++ gen/br/com/sparksoftware/mesadebar/R.java
@@ -10,63 +10,80 @@ package br.com.sparksoftware.mesadebar;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int adicionar=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int ImageViewAddPedidos=0x7f070010;
-        public static final int LinearLayoutPedidos=0x7f07000d;
-        public static final int TextView01=0x7f070002;
-        public static final int TextViewPedidos=0x7f07000e;
-        public static final int btnCancelar=0x7f070006;
-        public static final int btnGravar=0x7f070007;
-        public static final int edtNome=0x7f070001;
-        public static final int edtQuantidade=0x7f070003;
-        public static final int edtValorUnitario=0x7f070005;
-        public static final int imageView1=0x7f070012;
-        public static final int imageViewAddPessoas=0x7f07000a;
-        public static final int linearLayoutMesa=0x7f070008;
-        public static final int linearLayoutPessoas=0x7f070009;
-        public static final int listViewPedidos=0x7f07000f;
-        public static final int listViewPessoas=0x7f07000c;
-        public static final int menu_settings=0x7f070014;
-        public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070004;
-        public static final int textViewPessoas=0x7f07000b;
-        public static final int tvNomeItem=0x7f070011;
-        public static final int tvNomePessoa=0x7f070013;
+        public static final int ImageViewAddPedidos=0x7f080010;
+        public static final int LinearLayoutPedidos=0x7f08000d;
+        public static final int TextView01=0x7f080002;
+        public static final int TextViewPedidos=0x7f08000e;
+        public static final int btnCancelar=0x7f080006;
+        public static final int btnDividirConta=0x7f080011;
+        public static final int btnFechar=0x7f080015;
+        public static final int btnGravar=0x7f080007;
+        public static final int edtNome=0x7f080001;
+        public static final int edtQuantidade=0x7f080003;
+        public static final int edtValorUnitario=0x7f080005;
+        public static final int imageView1=0x7f080017;
+        public static final int imageViewAddPessoas=0x7f08000a;
+        public static final int linearLayoutMesa=0x7f080008;
+        public static final int linearLayoutPessoas=0x7f080009;
+        public static final int listViewDivisaoPedidoItens=0x7f080012;
+        public static final int listViewDivisaoPessoas=0x7f080014;
+        public static final int listViewPedidos=0x7f08000f;
+        public static final int listViewPessoas=0x7f08000c;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080013;
+        public static final int textViewPessoas=0x7f08000b;
+        public static final int tvNomeItem=0x7f080016;
+        public static final int tvNomePessoa=0x7f080018;
     }
     public static final class layout {
         public static final int activity_adicionar_pedido=0x7f030000;
         public static final int activity_adicionar_pessoa=0x7f030001;
         public static final int activity_principal=0x7f030002;
-        public static final int adapter_pedido_item=0x7f030003;
-        public static final int adapter_pessoa_item=0x7f030004;
+        public static final int activity_resultado_divisao=0x7f030003;
+        public static final int adapter_pedido_item=0x7f030004;
+        public static final int adapter_pessoa_item=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_adicionar_pedido=0x7f060000;
-        public static final int activity_adicionar_pessoa=0x7f060001;
-        public static final int activity_principal=0x7f060002;
+        public static final int resultado_divisao=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int buttonCancelar=0x7f04000a;
-        public static final int buttonGravar=0x7f040009;
-        public static final int hello_world=0x7f040001;
-        public static final int labelItem=0x7f04000c;
-        public static final int labelNome=0x7f04000b;
-        public static final int labelPedidos=0x7f040006;
-        public static final int labelPessoas=0x7f040004;
-        public static final int labelQuantidade=0x7f04000d;
-        public static final int labelValorUnitario=0x7f04000e;
-        public static final int lebelMesa=0x7f040005;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_adicionar_pedido=0x7f040008;
-        public static final int title_activity_adicionar_pessoa=0x7f040007;
-        public static final int title_activity_principal=0x7f040003;
+        public static final int action_settings=0x7f050010;
+        public static final int app_name=0x7f050000;
+        public static final int buttonCancelar=0x7f05000a;
+        public static final int buttonGravar=0x7f050009;
+        public static final int hello_world=0x7f050001;
+        public static final int labelDivisaoConta=0x7f050011;
+        public static final int labelItem=0x7f05000c;
+        public static final int labelNome=0x7f05000b;
+        public static final int labelPedidos=0x7f050006;
+        public static final int labelPessoas=0x7f050004;
+        public static final int labelQuantidade=0x7f05000d;
+        public static final int labelValorUnitario=0x7f05000e;
+        public static final int lebelMesa=0x7f050005;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_adicionar_pedido=0x7f050008;
+        public static final int title_activity_adicionar_pessoa=0x7f050007;
+        public static final int title_activity_principal=0x7f050003;
+        public static final int title_activity_resultado_divisao=0x7f05000f;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
